6eeba3d1bc9b71fd768356ce12e63e0d628e0f94
Don Park <donpark@docuverse.com>
2020-06-29 13:34:54 -0700

    simplify NPM published package

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 # experimental skinny publisher
+yarn clean
 yarn build
 rm -rf tmp-publish
 mkdir tmp-publish
 cp -R dist/* tmp-publish
+cp README.md LICENSE tmp-publish
 cp publish-package.json tmp-publish/package.json
 pushd tmp-publish
 npm publish
 popd
 rm -rf tmp-publish
-
