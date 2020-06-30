7f5fc371135a2c9fe67695c8b68a538465760acc
Don Park <donpark@docuverse.com>
2020-06-30 12:22:57 -0700

    allow element to be used without being inserted

--- src/TalkRecorder.js
+++ src/TalkRecorder.js
@@ -36,38 +36,7 @@ export class TalkRecorder extends HTMLElement {
 
     // Callback to notify custom element has been inserted into document.
     connectedCallback() {
-        if (this.host) {
-            this.service = new TalkRemoteService();
-            this.iframe = this.service.createServiceFrame(this);
-            this.appendChild(this.iframe);
-            this.iframePort = new FramePort(this.iframe);
-        } else {
-            this.service = new TalkLocalService();
-
-            if (this.role === 'framed' && parent !== window) {
-                this.parentPort = new FramePort(parent);
-                this.parentPort.addEventListener('record', e => {
-                    const msg = e.detail;
-                    this.record(msg.options)
-                });
-                this.parentPort.addEventListener('stop', e => {
-                    const msg = e.detail;
-                    this.stop(msg.reason);
-                });
-                this.parentPort.addEventListener('convert', e => {
-                    const msg = e.detail;
-                    this.convert(msg.blob, msg.options);
-                });
-                this.addEventListener('recorded', e => {
-                    const { blob } = e.detail;
-                    this.parentPort.postMessage({ type: 'recorded', blob })
-                })
-                this.addEventListener('converted', e => {
-                    const { blob } = e.detail;
-                    this.parentPort.postMessage({ type: 'converted', blob });
-                })
-            }
-        }
+        ensureInit.apply(this);
     }
 
     // Callback to notify custom element has been removed from document.
@@ -88,6 +57,8 @@ export class TalkRecorder extends HTMLElement {
     }
 
     async record(options = {}) {
+        ensureInit.apply(this);
+
         options = Object.assign({}, {
             type: 'opus',
         }, options);
@@ -100,10 +71,53 @@ export class TalkRecorder extends HTMLElement {
     }
 
     async stop(reason = 'finish') {
-        return this.service.stop(this, reason);
+        if (this.service) {
+            return this.service.stop(this, reason);
+        }
     }
 
     async convert(audioBlob, options = {}) {
+        ensureInit.apply(this);
         return this.service.convert(this, audioBlob, options);
+    }
+}
+
+// On-demand initialization allows class to be used without inserted.
+function ensureInit() {
+    if (this._inited) {
+        return;
+    }
+    this._inited = true;
+    if (this.host) {
+        this.service = new TalkRemoteService();
+        this.iframe = this.service.createServiceFrame(this);
+        this.appendChild(this.iframe);
+        this.iframePort = new FramePort(this.iframe);
+    } else {
+        this.service = new TalkLocalService();
+
+        if (this.role === 'framed' && parent !== window) {
+            this.parentPort = new FramePort(parent);
+            this.parentPort.addEventListener('record', e => {
+                const msg = e.detail;
+                this.record(msg.options)
+            });
+            this.parentPort.addEventListener('stop', e => {
+                const msg = e.detail;
+                this.stop(msg.reason);
+            });
+            this.parentPort.addEventListener('convert', e => {
+                const msg = e.detail;
+                this.convert(msg.blob, msg.options);
+            });
+            this.addEventListener('recorded', e => {
+                const { blob } = e.detail;
+                this.parentPort.postMessage({ type: 'recorded', blob })
+            })
+            this.addEventListener('converted', e => {
+                const { blob } = e.detail;
+                this.parentPort.postMessage({ type: 'converted', blob });
+            })
+        }
     }
 }
